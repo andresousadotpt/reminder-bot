a2e572ae7fd3fea4fd2d8eed0f48e9d341b11500
André Sousa <andre.sousa@andresousa.pt>
2024-06-03 20:02:05 +0100

    feat: Start project

---
+++ src/main/java/handler/Handler.java
@@ -0,0 +1,10 @@
+package handler;
+
+/**
+ * Handler class
+ */
+public final class Handler {
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
